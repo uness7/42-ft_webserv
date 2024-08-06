ceb7bb5c969531ce033953b9bc6e9aed626dfc30
Youness Zioual <yzioual@c2r6p6.42lehavre.fr>
2024-08-06 12:14:39 +0200

    zioual: CGI updated

--- cgi-bin/file_upload/upload.py
+++ cgi-bin/file_upload/upload.py
@@ -2,22 +2,22 @@
 
 import os
 import cgi
-import cgitb
 
-cgitb.enable()
 
 
 form = cgi.FieldStorage()
+print(form);
 
+"""
 for key in form.keys():
     print(f"{key}: {form[key]}")
 
-file_item = form['file']
-
+#file_item = form['file']
 if file_item.file:
     message = f'The file "{fn}" was uploaded successfully'
 else:
     message = 'No file was uploaded'
 
 print(f"Content-Type: text/html\n\n<html><body><p>{message}</p></body></html>")
+"""
 
